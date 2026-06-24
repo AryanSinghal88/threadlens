01bd16de5b5c55aa43b0632dd569843e40aafccb
AryanSinghal88 <aryansinghal783@gmail.com>
2026-06-24 12:54:23 +0530

    add user and analysis models

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import connectDB from './config/db.js'
+import dotenv from 'dotenv'
+import cors from 'cors'
+// import authRoutes from './routes/authRoutes.js'
+// import analysisRoutes from './routes/analysisRoutes.js'
+
+dotenv.config()
+
+const app = express()
+connectDB()
+app.use(cors())
+app.use(express.json())
+
+// app.use('/api/auth', authRoutes)
+// app.use('/api/analysis', analysisRoutes)
+
+app.get('/', (req, res)=>{
+    res.json({ 'message': 'Welcome to the Threadlens API' })
+})
+
+const PORT = process.env.PORT || 5175
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
