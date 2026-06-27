53a1e45ce094f2fc8b40a6ea94f2ac66780d4dba
Aryan Singhal <115364333+AryanSinghal88@users.noreply.github.com>
2026-06-27 23:17:28 +0530

    Rename API route from '/api/analysis' to '/api/analyses'

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(cors({
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
-app.use('/api/analysis', analysisRoutes)
+app.use('/api/analyses', analysisRoutes)
 
 app.get('/', (req, res)=>{
     res.json({ 'message': 'Welcome to the Threadlens API' })
